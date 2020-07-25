9b7d18d67f10514c279ac8a6317c17f5ae928948
Adarsh Kumar <adarshkumar9820@gmail.com>
2020-07-25 13:59:23 +0800

    indexer

--- src/main/java/com/lucifier/app/indexer/ProductIndexer.java
+++ src/main/java/com/lucifier/app/indexer/ProductIndexer.java
@@ -7,6 +7,7 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,9 +16,8 @@ public class ProductIndexer extends RootIndexer implements Indexer {
 
   @Override
   public void index(List<Product> products) throws IOException {
-
+    IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
     IndexWriter writer = new IndexWriter(memoryIndex, indexWriterConfig);
-
     for (Product p : products) {
       Document document = new Document();
       document.add(new TextField("title", p.getTitle(), Field.Store.YES));
--- src/main/java/com/lucifier/app/indexer/RootIndexer.java
+++ src/main/java/com/lucifier/app/indexer/RootIndexer.java
@@ -1,7 +1,6 @@
 package com.lucifier.app.indexer;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 
@@ -9,11 +8,10 @@ public class RootIndexer {
 
   Directory memoryIndex;
   StandardAnalyzer analyzer;
-  IndexWriterConfig indexWriterConfig;
+
 
   RootIndexer() {
     memoryIndex = new RAMDirectory();
     analyzer = new StandardAnalyzer();
-    indexWriterConfig = new IndexWriterConfig(analyzer);
   }
 }
