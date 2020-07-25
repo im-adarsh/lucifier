2ea832eb8df068b4139662056f28f49d3ec4907f
Adarsh Kumar <adarshkumar9820@gmail.com>
2020-07-25 13:04:33 +0800

    init searcher controller

--- src/main/java/com/lucifier/rest/SearchController.java
+++ src/main/java/com/lucifier/rest/SearchController.java
@@ -3,8 +3,8 @@ package com.lucifier.rest;
 import com.lucifier.entity.Product;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/searcher")
 public class SearchController {
 
-  @PostMapping("/products")
-  public ResponseEntity<List<Product>> indexProduct(@RequestBody List<Product> products) {
-    return ResponseEntity.ok().body(products);
+  @PostMapping("/products/query/{query}")
+  public ResponseEntity<List<Product>> indexProduct(@PathVariable String query) {
+    return ResponseEntity.ok().body(null);
   }
 }
